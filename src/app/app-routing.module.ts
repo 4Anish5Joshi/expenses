5f44a7dd6b66b15e53a203b2c387969ee4f8ae6b
Anish <anishdjoshi4@gmail.com>
2024-04-09 10:13:14 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TrackerComponent } from './tracker/tracker.component';
+import { VisualizationComponent } from './visualization/visualization.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: TrackerComponent },
+  { path: 'add', component: TrackerComponent },
+  { path: 'analytics', component: VisualizationComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
